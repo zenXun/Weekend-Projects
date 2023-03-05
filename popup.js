e20131f832c8121de419902f86b5da5ccaa4d885
Zac Wu <wukey1992@gmail.com>
2023-03-05 15:28:55 -0700

    moved css to a separated file

--- popup.js
+++ popup.js
@@ -11,7 +11,7 @@ reviseButton.onclick = function (element) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer <API KEY>',
+      'Authorization': 'Bearer <API Key>',
     },
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
